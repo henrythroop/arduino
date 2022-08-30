d5e1976bb17d427645defb3a0afef7d9478b6407
Henry Throop <henry.throop@gmail.com>
2022-08-30 15:47:00 +0530

    car minor edits

--- car_arh/car_arh.ino
+++ car_arh/car_arh.ino
@@ -56,15 +56,11 @@ void playSong(){
 
 void playNote(char note, int duration) {
   char names[] = {'C', 'D', 'E', 'F', 'G', 'A', 'B',           
-
                  'c', 'd', 'e', 'f', 'g', 'a', 'b',
-
                  'x', 'y' };
 
   int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014,
-
                  956,  834,  765,  593,  468,  346,  224,
-
                  655 , 715 };
 
   int SPEE = 5;
